46c5096c4d371efbc45165cd506348e4cc8604d8
AR-Ovi <85396348+AR-Ovi@users.noreply.github.com>
2022-03-07 21:43:18 +0600

    Responsive Update

---
+++ scripts/responsive.script.js
@@ -0,0 +1,4 @@
+if (screen.width <= 800)
+      {document.write("<link href='./style/mobile.800.css' rel='stylesheet' type='text/css'>")}
+else
+      {document.write("<link href='./style/style.css' rel='stylesheet' type='text/css'>")}
