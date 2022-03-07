81360a35ab0fa660bca5f9f943431f98347b79e6
AR-Ovi <85396348+AR-Ovi@users.noreply.github.com>
2022-03-07 21:42:20 +0600

    remake

--- scripts/responsive.script.js
+++
@@ -1,4 +0,0 @@
-if (screen.width <= 800)
-      {document.write("<link href='./style/mobile.800.css' rel='stylesheet' type='text/css'>")}
-else
-      {document.write("<link href='./style/style.css' rel='stylesheet' type='text/css'>")}
